eea9f6250236fa3d388fd26bae6ae6a28e43aecc
ChineDmitri <mr.dima11@gmail.com>
2024-04-02 17:08:09 +0200

    Modelisation ok

--- src/main/scala/td3et4/T1Bases4pts.scala
+++ src/main/scala/td3et4/T1Bases4pts.scala
@@ -11,7 +11,7 @@ object Bases4pts extends App {
 
   /* 1. Afficher les 10 premiers entiers et leur valeur au carré
    en utilisant Range et foreach (0.5pt) */
-  Functions.getFirst10IntAndPowForEach()
+  FunctionsT1.getFirst10IntAndPowForEach()
 
   /* 2.1. Créer une fonction firstTwo(s: Seq[Int]): (Int, Int)
   qui renvoie les deux premiers éléments d’une liste sous forme de tuple
@@ -19,22 +19,22 @@ object Bases4pts extends App {
   /* 2.2 OPTIONAL (si j'airrive) -> Transformer cette fonction pour être générique
   et marcher avec tout type de Seq (0.5pt) */ 4
   val seqInt = Seq(1, 11, 2, 22, 3, 33, 4, 44)
-  val tupleInt: (Int, Int) = Functions.firstTwo(seqInt)
+  val tupleInt: (Int, Int) = FunctionsT1.firstTwo(seqInt)
   println(tupleInt)
-  val tupleStr: (String, String) = Functions.firstTwo(Seq("hello", "world", "Scala"))
+  val tupleStr: (String, String) = FunctionsT1.firstTwo(Seq("hello", "world", "Scala"))
   println(tupleStr)
-  val tupleDouble: (Double, Double) = Functions.firstTwo(Seq(3.14, 2.71, 1.41, 1.61))
+  val tupleDouble: (Double, Double) = FunctionsT1.firstTwo(Seq(3.14, 2.71, 1.41, 1.61))
   println(tupleDouble)
 
   /* 3. Créer une fonction createSandwich(s: Seq[Int]): (Int, Seq[Int], Int)
   qui renvoie un tuple avec le premier élément de la liste,
   une sous-liste des éléments au milieu de la liste,
   et le dernier élément de la liste. (0.5pt) */
-  val sandwich: (Int, Seq[Int], Int) = Functions.createSandwich(seqInt)
+  val sandwich: (Int, Seq[Int], Int) = FunctionsT1.createSandwich(seqInt)
   println(sandwich)
 }
 
-case object Functions {
+case object FunctionsT1 {
   def getFirst10IntAndPowForEach(): Unit = {
     println("getFirst10IntAndPowForEach")
     (1 to 10).foreach { num =>
--- src/main/scala/td3et4/T2SalleDeClasse6pts.scala
+++ src/main/scala/td3et4/T2SalleDeClasse6pts.scala
@@ -1,6 +1,54 @@
 package fr.esgi.funcProg
 package td3et4
 
+import java.util.UUID
+
 object T2SalleDeClasse6pts extends App {
 
+  val room = FunctionsT2.createAndReturneClasseAvecEtudiants()
+
+  val etudiantEFromClasse = room.get(1)(1) // Obtient l'élève E
+  val etudiantDFromClasse = room.get(1)(0) // Obtient l'élève D
+  println(s"${etudiantEFromClasse.id} : nom : ${etudiantEFromClasse.nom}, l'age : ${etudiantEFromClasse.age}")
+  println(s"${etudiantDFromClasse.id} : nom : ${etudiantDFromClasse.nom}, l'age : ${etudiantDFromClasse.age}")
+}
+
+case object FunctionsT2 {
+
+  def createAndReturneClasseAvecEtudiants(): Room = {
+    val studentA = Student("Alpha", 18)
+    val studentB = Student("Bravo", 17)
+    val studentC = Student("Charlie", 16)
+    val range1 = List(studentA, studentB, studentC)
+
+    val studentD = Student("Delta", 18)
+    val studentE = Student("Echo", 17)
+    val studentF = Student("Foxtrot", 16)
+    val range2 = List(studentD, studentE, studentF)
+
+    val room = Room(
+      range1,
+      range2
+    )
+
+    room
+  }
+
+}
+
+/*1.1 Construire une case class Student pour modéliser un élève*/
+case class Student(nom: String, age: Int) {
+  val id: String = UUID.randomUUID().toString
+}
+
+/*1.1 Construire une case class Room pour la salle de cours
+(on considère qu'une salle est une liste de rangs, chaque rang étant une liste d'élèves  )*/*
+/*Utilisation uperateur * pour avoir N liste de etudiant dans parametres.*/
+case class Room(students: List[Student]*) {
+
+  /* 1.2 Proposer une méthode pour récupérer un élève en fonction de sa position.
+  Exemple room.get(2)(1) = Student(E) */
+  def get(row: Int)(seat: Int): Student = {
+    students(row)(seat)
+  }
 }
